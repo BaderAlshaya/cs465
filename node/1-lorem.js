ae2c5ad57bd5ce54f7134221111cc34385a671d6
Bader Alshaya <alshayabader@gmail.com>
2019-02-06 15:19:27 -0800

    node exercise 1 completed

--- node/1-lorem.js
+++ node/1-lorem.js
@@ -3,3 +3,10 @@
 var http = require('http'); // do not change this line
 
 // any request should return '<!DOCTYPE html><html><body>lorem ipsum</body></html>' as html
+var server = http.createServer(function(req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write('<!DOCTYPE html><html><body>lorem ipsum</body></html>');
+  res.end();
+});
+
+server.listen(process.env.PORT || 8080);
