7c4c5ae4b99126f093ab841bd9db6dcf52d2e842
Bader Alshaya <alshayabader@gmail.com>
2019-02-06 23:14:51 -0800

    node exercise 6 completed

--- node/6-dns.js
+++ node/6-dns.js
@@ -4,7 +4,25 @@ var http = require('http'); // do not change this line
 var dns = require('dns'); // do not change this line
 
 // http://localhost:8080/pdx.edu should return '131.252.115.150' in plain text (address might be different, there could even be multiple addresses)
-
 // http://localhost:8080/sniklaus.com should return '216.239.36.21\n216.239.38.21\n216.239.32.21\n216.239.34.21' in plain text (addresses / order might be different)
-
 // http://localhost:8080/error should return 'error' in plain text
+
+var server = http.createServer(function(req, res) {
+  dns.resolve4(unescape(req.url.substr(1)), function(error, address) {
+    if (error) {
+      res.writeHead(200, {
+        'Content-Type': 'text/plain'
+      });
+      res.write('error');
+      res.end();
+    } else {
+      res.writeHead(200, {
+        'Content-Type': 'text/plain'
+      });
+      res.write(address.join('\n'));
+      res.end();
+    }
+  });
+});
+
+server.listen(process.env.PORT || 8080);
