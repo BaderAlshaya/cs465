af503369cabfa269d61e15456f866ebed3da0a93
Bader Alshaya <alshayabader@gmail.com>
2019-02-11 14:33:54 -0800

    express exercise 1 completed

--- express/1-lorem.js
+++ express/1-lorem.js
@@ -3,3 +3,16 @@
 var express = require('express'); // do not change this line
 
 // http://localhost:8080/lorem should return '<!DOCTYPE html><html><body>lorem ipsum</body></html>' as html
+
+var server = express();
+server.get('/lorem', function(req, res) {
+  res.status(200);
+  res.set({
+    'Content-Type': 'text/html'
+  });
+
+  res.write('<!DOCTYPE html><html><body>lorem ipsum</body></html>');
+  res.end();
+});
+
+server.listen(process.env.PORT || 8080);
